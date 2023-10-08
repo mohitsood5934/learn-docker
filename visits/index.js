3e98387ffa3f605492f46e563870283466be9bb6
Mohit Sood <mohit.sood@justdial.com>
2023-10-08 10:46:04 +0530

    modify yaml file

--- visits/index.js
+++ visits/index.js
@@ -3,21 +3,23 @@ const redis = require("redis");
 
 const app = express();
 
-const client = redis.createClient({
-    host: 'redis-server', // service  name in docker-compose file
-    port: 6379,
-    legacyMode: true,
-});
-client.connect().catch(console.error)
-client.set('visits', 0);
-
+// const client = redis.createClient({
+//     host: 'redis-server', // service  name in docker-compose file
+//     port: 6379,
+//     legacyMode: true,
+// });
+// client.connect().catch(console.error)
+// client.set('visits', 0);
+let visits = 0;
 app.get("/", (req, res) => {
-  client.get("visits", (err, visits) => {
+  console.log('hit this route');
+  // client.get("visits", (err, visits) => {
     res.send(`Number of visits is ${visits}`);
-    client.set("visits", parseInt(visits) + 1);
-  });
+    visits += 1;
+  //   client.set("visits", parseInt(visits) + 1);
+  // });
 });
 
 app.listen(4001, () => {
-  console.log("listening on port 8081");
+  console.log("listening on port 4001");
 });
